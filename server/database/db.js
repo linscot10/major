5454098cf5889ddf272d0c67a20139ff006d4364
linscot10 <linscot10@gmail.com>
2025-02-02 14:05:24 +0300

    setting up user Schema

--- server/database/db.js
+++ server/database/db.js
@@ -1,13 +1,14 @@
 const mongoose = require("mongoose")
+const colors = require("colors")
 require('dotenv').config()
 
 const connectDB = async () => {
     try {
         await mongoose.connect(process.env.MONGO_URI)
-        console.log("Mongodb is connected Succesfully !")
+        console.log(`Mongodb is connected Succesfully !`.bgCyan.white)
 
     } catch (error) {
-        console.error("problem connecting to the database", error)
+        console.error(`problem connecting to the database ${error}`.bgRed.white)
         process.exit(1)
     }
 }
