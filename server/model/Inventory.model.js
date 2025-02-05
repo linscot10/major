d92a9fa5a6172e852cc12e9bc487b8e14256d1e7
linscot10 <linscot10@gmail.com>
2025-02-05 23:28:09 +0300

    inventory Schema

--- server/model/Inventory.model.js
+++ server/model/Inventory.model.js
@@ -10,7 +10,31 @@ const inventorySchema = new mongoose.Schema({
         type: String,
         require: [true, "Blodd Group required"],
         enum: ['O+', 'O-', 'AB+', 'AB-', 'A+', 'A-', 'B+', 'B-']
+    },
+    quantinty: {
+        type: Number,
+        require: [true, 'blood quantity is required']
+    },
+    organisation: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'users',
+        require: [true, 'organisation is required']
+    },
+    hospital: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'users',
+        require: function () {
+            return this.inventoryTpe === "out"
+        }
+    },
+    donor: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'users',
+        require: function () {
+            return this.inventoryTpe === "in"
+        }
     }
-})
+
+},{timestamps:true})
 
 model.exports = mongoose.model("Inventory", inventorySchema)
