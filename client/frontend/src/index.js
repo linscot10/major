032353d8ee1cd47c12c12a36d3e1531dcc60f8bd
linscot10 <linscot10@gmail.com>
2025-02-11 14:15:09 +0300

    redux toolkit

--- client/frontend/src/index.js
+++ client/frontend/src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom'
 import './index.css';
 import App from './App';
+import { Provider } from 'react-redux'
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Provider>
 
+    </Provider>
     <BrowserRouter>
       <App />
     </BrowserRouter>
