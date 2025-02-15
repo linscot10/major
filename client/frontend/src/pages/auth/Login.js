0809226fc958568c25768a44cbbf1aa675eaa40a
linscot10 <linscot10@gmail.com>
2025-02-15 20:24:44 +0300

    Spinner

--- client/frontend/src/pages/auth/Login.js
+++ client/frontend/src/pages/auth/Login.js
@@ -1,23 +1,33 @@
 import React from 'react'
 import Form from '../../components/shared/form/Form'
+import { useSelector } from 'react-redux'
+import Spinner from '../../components/shared/Spinner'
 
 
 const Login = () => {
+    const { loading, error } = useSelector(state => state.auth)
     return (
-        <>
-            <div className="row g-0">
-                <div className="col-md-8 form-banner">
-                    <img src="./assets/images/banner.jpg" alt="Login Image" />
-                </div>
-                <div className="col-md-4 form-container">
-                    <Form
-                        formTitle={"Login Page"}
-                        submitBtn={'Login'}
-                        formType={'login'}
-                    />
 
-                </div>
-            </div>
+        <>
+            {
+                error && <span>{alert(error)}</span>
+            }
+            {
+
+                loading ? (< Spinner />) : (<div className="row g-0">
+                    <div className="col-md-8 form-banner">
+                        <img src="./assets/images/banner.jpg" alt="Login Image" />
+                    </div>
+                    <div className="col-md-4 form-container">
+                        <Form
+                            formTitle={"Login Page"}
+                            submitBtn={'Login'}
+                            formType={'login'}
+                        />
+
+                    </div>
+                </div>)
+            }
         </>
     )
 }
