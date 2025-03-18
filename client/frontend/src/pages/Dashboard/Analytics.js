102c242c48950415d63de65f3c60741d6a9c0a61
linscot10 <linscot10@gmail.com>
2025-03-18 22:34:14 +0300

    analytics

--- client/frontend/src/pages/Dashboard/Analytics.js
+++ client/frontend/src/pages/Dashboard/Analytics.js
@@ -1,11 +1,42 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Header from '../../components/shared/layout/Header'
-
+import Api from '../../services/Api'
 const Analytics = () => {
+    const [data, setData] = useState([])
+
+    const getBloodGroupData = async () => {
+        try {
+            const { data } = await Api.get('/analytics/bloodGroup-data')
+            if (data?.success) {
+                setData(data?.bloodGroupData)
+                console.log(data)
+            }
+
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    useEffect(() => {
+        getBloodGroupData();
+    }, [])
     return (
         <>
             <Header />
-            <h1>Analytics</h1>
+            <div class="d-flex flex-row flex-wrap">
+                {
+                    data?.map(record => (
+                        <div className="card" style={{ width: '18rem' }}>
+                            <div className="card-body">
+                                <h5 className="card-title">Card title</h5>
+                                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the
+                                    card's content.</p>
+
+                            </div>
+                        </div>
+                    ))
+                }
+            </div >
         </>
     )
 }
